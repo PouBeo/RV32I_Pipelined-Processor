8aecc93fcd56366eae8c774111b806cc1dae8c81
Quan_NgynMnh <128219437+PouBeo@users.noreply.github.com>
2024-08-15 20:15:26 +0700

    Update forward_rs_mux.sv

--- src/forward_rs_mux.sv
+++ src/forward_rs_mux.sv
@@ -7,11 +7,9 @@ module forward_rs_mux (
   input  logic [31:0]  MEM_aludata_i,
   output logic [31:0]  rs_data_o
 );
-/*  assign rs_data_o = ( forward_sel_i == 2'b00 ) ? EX_rsdata_i :
-                     ( forward_sel_i == 2'b01 ) ? MEM_aludata_i :
-                     ( forward_sel_i == 2'b10 ) ? WB_wbdata_i : 32'bx ; */
 
   assign rs_data_o = ( forward_sel_i == `NOforward  ) ? EX_rsdata_i :
                      ( forward_sel_i == `forwardMEM ) ? MEM_aludata_i :
                      ( forward_sel_i == `forwardWB  ) ? WB_wbdata_i : 32'bx ;
+
 endmodule: forward_rs_mux
